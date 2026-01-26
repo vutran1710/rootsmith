a71e1165582d0090eefd0df438cfae9670b3f330
vutran1710 <me@vutr.io>
2026-01-26 10:08:40 +0700

    clean up

--- src/archiver/variant.rs
+++ src/archiver/variant.rs
@@ -2,8 +2,6 @@ use anyhow::Result;
 use async_trait::async_trait;
 
 use super::file::FileArchive;
-use super::mock::MockArchive;
-use super::noop::NoopArchive;
 use super::s3_glacier::S3GlacierArchive;
 use crate::traits::ArchiveData;
 use crate::traits::ArchiveFilter;
@@ -11,8 +9,6 @@ use crate::traits::ArchiveStorage;
 
 /// Enum representing all possible archive storage implementations.
 pub enum ArchiveStorageVariant {
-    Noop(NoopArchive),
-    Mock(MockArchive),
     S3Glacier(S3GlacierArchive),
     File(FileArchive),
 }
@@ -21,8 +17,6 @@ pub enum ArchiveStorageVariant {
 impl ArchiveStorage for ArchiveStorageVariant {
     fn name(&self) -> &'static str {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.name(),
-            ArchiveStorageVariant::Mock(inner) => inner.name(),
             ArchiveStorageVariant::S3Glacier(inner) => inner.name(),
             ArchiveStorageVariant::File(inner) => inner.name(),
         }
@@ -30,8 +24,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn archive(&self, data: &ArchiveData) -> Result<String> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.archive(data).await,
-            ArchiveStorageVariant::Mock(inner) => inner.archive(data).await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.archive(data).await,
             ArchiveStorageVariant::File(inner) => inner.archive(data).await,
         }
@@ -39,8 +31,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn archive_batch(&self, items: &[ArchiveData]) -> Result<Vec<String>> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.archive_batch(items).await,
-            ArchiveStorageVariant::Mock(inner) => inner.archive_batch(items).await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.archive_batch(items).await,
             ArchiveStorageVariant::File(inner) => inner.archive_batch(items).await,
         }
@@ -48,8 +38,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn retrieve(&self, archive_id: &str) -> Result<Option<ArchiveData>> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.retrieve(archive_id).await,
-            ArchiveStorageVariant::Mock(inner) => inner.retrieve(archive_id).await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.retrieve(archive_id).await,
             ArchiveStorageVariant::File(inner) => inner.retrieve(archive_id).await,
         }
@@ -57,8 +45,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn query(&self, filter: &ArchiveFilter) -> Result<Vec<String>> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.query(filter).await,
-            ArchiveStorageVariant::Mock(inner) => inner.query(filter).await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.query(filter).await,
             ArchiveStorageVariant::File(inner) => inner.query(filter).await,
         }
@@ -66,8 +52,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn delete(&self, archive_id: &str) -> Result<bool> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.delete(archive_id).await,
-            ArchiveStorageVariant::Mock(inner) => inner.delete(archive_id).await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.delete(archive_id).await,
             ArchiveStorageVariant::File(inner) => inner.delete(archive_id).await,
         }
@@ -75,8 +59,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn open(&mut self) -> Result<()> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.open().await,
-            ArchiveStorageVariant::Mock(inner) => inner.open().await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.open().await,
             ArchiveStorageVariant::File(inner) => inner.open().await,
         }
@@ -84,8 +66,6 @@ impl ArchiveStorage for ArchiveStorageVariant {
 
     async fn close(&mut self) -> Result<()> {
         match self {
-            ArchiveStorageVariant::Noop(inner) => inner.close().await,
-            ArchiveStorageVariant::Mock(inner) => inner.close().await,
             ArchiveStorageVariant::S3Glacier(inner) => inner.close().await,
             ArchiveStorageVariant::File(inner) => inner.close().await,
         }
